0c52a200e8578be394f2f06794544e0e4d65e9da
FTCODfit <r0976730328@gmail.com>
2026-01-06 20:32:54 +0800

    Update discordClient.py

--- discordClient.py
+++ discordClient.py
@@ -1,15 +1,16 @@
 import requests
 import os
+import json
 from dotenv import load_dotenv
 from dataclasses import dataclass
 
-@dataclass
+@dataclass(slots=True)
 class DiscordAuthor:
     id: str
     username: str
     global_name: str | None
     
-@dataclass
+@dataclass(slots=True)
 class DiscordMessage:
     msg_id: str
     content: str
@@ -21,23 +22,22 @@ class DiscordClient:
     def __init__(self):
         load_dotenv()
         self.API = self.API(self)
+        self._url = None
         self._last_msg_id = 0
+        
         
         if not os.getenv("DISCORD_TOKEN"):
             raise RuntimeError("DISCORD_TOKEN not set")
         self._HEADERS = {
-            "Authorization": os.getenv("DISCORD_TOKEN"),
-            "Content-Type": "application/json"}
+            "Authorization": os.getenv("DISCORD_TOKEN")}
         
         
-    def get_msg(self, channel_id, get_limit=10, url=None):
+    def _get_msg(self, channel_id, get_limit=10, url=None):
         if not url:
             url = f'https://discord.com/api/v9/channels/{channel_id}/messages?limit={get_limit}'
         response = requests.get(url, headers=self._HEADERS)
-
         response.raise_for_status()
 
-        
         messages = response.json()
         messages.reverse()
         new_messages: list[DiscordMessage] = []
@@ -67,12 +67,42 @@ class DiscordClient:
             
         return new_messages
 
+    def _send_msg(self, channel_id, message, attachment_path, url=None):
+        if not url:
+            url = f'https://discord.com/api/v9/channels/{channel_id}/messages'
+        
+        if attachment_path is None:
+            payload = {"content": message or ""}
+            response =  requests.post(url, headers=self._HEADERS, json=payload)
+            return
+        
+        if not isinstance(attachment_path, list):
+            attachment_path = [attachment_path]
+        
+        if len(attachment_path)>10: 
+            raise ValueError("Discord allows up to 10 attachments per message")
+        
+        payload_json = {"content": message or ""}
+        files = {}
+        attachment_handle = []
+        for idx, path in enumerate(attachment_path):
+            attachment = open(path, "rb")
+            files[f"files[{idx}]"] = (os.path.basename(path), attachment)
+            attachment_handle.append(attachment)
+        files["payload_json"] = (None, json.dumps(payload_json), "application/json")
 
+        response = requests.post(url, headers=self._HEADERS, files=files)
+        
+        for file in attachment_handle:
+            file.close()
+
+    
+    
     class API:
         def __init__(self, outer: "DiscordClient"):
             self.outer = outer
         
-        def fetch_new_msg(self, channel_id: str, msg_limit: int=10)-> list[DiscordMessage]:
+        def fetch_msg(self, channel_id: str, msg_limit: int=10)-> list[DiscordMessage]:
             """
             Fetch new messages from a Discord channel.
 
@@ -96,12 +126,35 @@ class DiscordClient:
                 - mention_ids: List of mentioned user IDs
                 - timestamp: ISO 8601 timestamp string
             """
-            return self.outer.get_msg(channel_id, msg_limit)
+            return self.outer._get_msg(channel_id, msg_limit)
         
+        def send_msg(self, channel_id: str, message:str|None=None, attachment_path:str|list[str]|None=None):
+            """
+            Send message and attachment to the Discord channel.
 
+            Parameters
+            ----------
+            channel_id : str
+                Discord channel ID to Send message or attachment to.
+            message : str, optional
+                Text content of the message.
+            attachment_path : str, list[str], optional
+                Path or paths to files to be set as attachment. 
+                Supported types include images, PDFs, and other files allowed by discord.
+            """
+            
+            self.outer._send_msg(channel_id, message, attachment_path)
+        
+        def reset_cursor(self):
+            """
+            Reset the message cursor used for fetching new messages.
+            """
+            self.outer._last_msg_id = 0
 # Example
 if __name__=="__main__":
     app = DiscordClient().API
-    msgs = app.fetch_new_msg("Discord channel ID")
+    discord_id = "Discord channel ID"
+    msgs = app.fetch_msg(discord_id)
     for msg in msgs:
         print(f"{msg.author.username} :", msg.content)
+    app.send_msg(discord_id, "message", ["filepath1", "filepath2"])
